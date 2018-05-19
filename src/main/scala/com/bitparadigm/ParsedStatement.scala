e0bc092363368c3330a828f0884b18a5dd086c73
Luke Simkins <luke@bitparadigm.com>
2018-05-18 22:09:13 -0500

    Move "parse" function into ProgramParser

--- src/main/scala/com/bitparadigm/ParsedStatement.scala
+++ src/main/scala/com/bitparadigm/ParsedStatement.scala
@@ -9,23 +9,3 @@ case class ParsedStatement(
   raw: String,
   context: String = ""
 )
-object ParsedStatement {
-  def parse(str: String): ParsedStatement = {
-    val parts = str.split(" ")
-    val command = CommandType.mapType(parts(0))
-
-    command match {
-      case (cmd: ArithmeticCommand) =>
-        ParsedStatement(cmd, raw=str)
-      case (cmd: BranchingCommand) =>
-        ParsedStatement(cmd, Some(parts(1)), raw=str)
-      case (cmd: MemorySegmentCommand) =>
-        ParsedStatement(
-          cmd,
-          Some(MemorySegment.mapMemorySegment(parts(1))),
-          Some(parts(2).toLong), // TODO: Add validation and error handling
-          raw=str)
-      case _ => throw TranslationError("Unrecognized command", new Throwable(str))
-    }
-  }
-}
--- src/main/scala/com/bitparadigm/ProgramParser.scala
+++ src/main/scala/com/bitparadigm/ProgramParser.scala
@@ -11,7 +11,7 @@ class ProgramParser(val lines: Iterator[String], context: String = "") {
 
   lazy val statements: Iterator[ParsedStatement] =
     rawStatements
-      .map(r => ParsedStatement.parse(r).copy(context = context))
+      .map(r => ProgramParser.parse(r).copy(context = context))
 
   private val nonCommandMatcher = raw"(?:/{2}).+|(?:)(?!.+)|(?:)[\r\n]+(?!.+)".r
   private def isStatement(line: String): Boolean = {
@@ -23,6 +23,23 @@ class ProgramParser(val lines: Iterator[String], context: String = "") {
 }
 
 object ProgramParser {
+  def parse(str: String): ParsedStatement = {
+    val parts = str.split(" ")
+    val command = CommandType.mapType(parts(0))
+
+    command match {
+      case (cmd: ArithmeticCommand) => ParsedStatement(cmd, raw=str)
+      case (cmd: BranchingCommand)  => ParsedStatement(cmd, Some(parts(1)), raw=str)
+      case (cmd: MemorySegmentCommand) =>
+        ParsedStatement(
+          cmd,
+          Some(MemorySegment.mapMemorySegment(parts(1))),
+          Some(parts(2).toLong), // TODO: Add validation and error handling
+          raw=str)
+      case _ => throw TranslationError("Unrecognized command", new Throwable(str))
+    }
+  }
+
   def fromFile(filename: String): Try[ProgramParser] = {
     val basename = filename.split("\\.").head
     Try(new ProgramParser(Source.fromFile(filename).getLines(), basename))
